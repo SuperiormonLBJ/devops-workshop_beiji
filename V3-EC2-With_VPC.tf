961e613475f1301d051f29fae90840eb5fa82d81
SuperiormonLBJ <superiormon187@gmail.com>
2023-09-02 12:01:33 +0800

    added gitignore and V3 to create VPC in aws

---
+++ V3-EC2-With_VPC.tf
@@ -0,0 +1,104 @@
+provider "aws" {
+  // change to sinagpore
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "demo-server_3" {
+    // change here to match the ami type in aws, this is amazon linux ami
+    ami = "ami-0464f90f5928bccb8"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    //security_groups = [ "demo-sg" ]   // here is only for create e2c
+    vpc_security_group_ids = [aws_security_group.demo-sg.id] // security way in vpc
+    // rightnow only call 1st subnet
+    subnet_id = aws_subnet.dpp-public-subnet-01.id 
+
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id 
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
+
+// way to create vpc in aws
+// resource type: aws_vpc,  resource name: dpp-vpc
+// resource is = 'dpp-vpc' in tags
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpp-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  // change to sinagpore
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "dpp-public-subent-01"
+  }
+}
+
+// 2nd subnet, can have many subnet inside 1 vpc
+resource "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  // change to sinagpore b
+  availability_zone = "ap-southeast-1b"
+  tags = {
+    Name = "dpp-public-subent-02"
+  }
+}
+
+// internet gateway
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id 
+  tags = {
+    Name = "dpp-igw"
+  } 
+}
+
+// route table
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id 
+  }
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+  subnet_id = aws_subnet.dpp-public-subnet-01.id
+  route_table_id = aws_route_table.dpp-public-rt.id   
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id = aws_subnet.dpp-public-subnet-02.id 
+  route_table_id = aws_route_table.dpp-public-rt.id   
+}
